9124c46f9203af3e07da5bf74dd5ee701a91a1eb
belaletech <belalahmad@lambdatest.com>
2024-09-13 18:57:34 +0530

    update webdriver & requirements.txt

--- simple_web_automation.py
+++ simple_web_automation.py
@@ -2,19 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-# Set up the Chrome WebDriver
-driver = webdriver.Chrome(ChromeDriverManager().install())
+
+# Set up the Chrome WebDriver with options
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=webdriver.chrome.service.Service(ChromeDriverManager().install()), options=options)
 
 # Open the login page
 driver.get("https://www.google.com/")
-driver.get("https://www.google.com/") #used to search web pages wit .get you can get into the links 
-driver.maximize_window() #maximizes the window
-time.sleep(3) #waits or stands for n secound for further command to execute
-driver.find_element("name", "q").send_keys("LambdaTest Login") 
-driver.find_element("name", "q").send_keys(Keys.ENTER) #find_element you can find by id,or you can find by xpath
+driver.maximize_window()
+time.sleep(3)
+
+# Search for LambdaTest Login
+driver.find_element("name", "q").send_keys("LambdaTest Login")
+driver.find_element("name", "q").send_keys(Keys.ENTER)
+
+# Click on the login link
 driver.find_element("partial link text", "Log in - LambdaTest").click()
-driver.find_element("id","email").send_keys("ritamg@lambdatest.com") #send keys is used to used to send elements log or files or text to the web app
-driver.find_element("id","password").send_keys("Shiva@12")
+
+# Enter email and password
+driver.find_element("id", "email").send_keys("ritamg@lambdatest.com")
+driver.find_element("id", "password").send_keys("Shiva@12")
+
+# Click login button
 driver.find_element("id", "login-button").click()
+
 time.sleep(10)
-driver.quit() #please always quit the driver so that it doent reaches ideal time out
+driver.quit()
